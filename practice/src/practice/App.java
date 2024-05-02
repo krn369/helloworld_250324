4e5208644c1ae907c5af0678ec18835145c8e8ef
krn369 <144631087+krn369@users.noreply.github.com>
2024-05-02 12:33:12 +0200

    update

---
+++ practice/src/practice/App.java
@@ -0,0 +1,40 @@
+package practice;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		//MINI PROJECT	
+		
+		Scanner sc = new  Scanner(System.in);
+		
+		int myNumber = (int)(Math.random()*100);
+		int userNumber = 0;
+		
+		do {
+			
+			System.out.println("Guess my number (1 - 100) : ");
+				userNumber = sc.nextInt();
+			
+			if (userNumber == myNumber ) {
+				System.out.println("WOOHOO! CORRECT NUMBER !!!");
+				break;
+			}
+			
+			else if (userNumber > myNumber) {
+				System.out.println("Your number is too large.");
+			}
+			else {
+				System.out.println("Your number is too small.");
+			}
+		} while (userNumber >= 0);
+		System.out.print("My number was : ");
+		System.out.println(myNumber);
+		
+		sc.close();
+		
+	}
+
+}
